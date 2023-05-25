bf9e402d7e0b079c175f98f3f595e96a56584b25
BlevWasTaken <75867467+BlevWasTaken@users.noreply.github.com>
2023-05-25 18:32:58 +0930

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ if (!firebase._apps.length) {
 const servers = {
   iceServers: [
     {
-      urls: "stun:a.relay.metered.ca:80",
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
     },
     {
       urls: "turn:relay1.expressturn.com:3478",
